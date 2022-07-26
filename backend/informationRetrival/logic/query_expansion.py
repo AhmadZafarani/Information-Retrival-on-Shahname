66e7223921824e7e3da7fa86f584e31d56e77e1f
AhmadZafarani <ahmadzafarani2013@gmail.com>
2022-07-27 02:22:16 +0430

    query expansion with tf-idf

--- backend/informationRetrival/logic/query_expansion.py
+++ backend/informationRetrival/logic/query_expansion.py
@@ -1,45 +1,101 @@
+import codecs
+import random
 from pathlib import Path
 
 import numpy as np
-from gensim.models.keyedvectors import KeyedVectors
+from numpy import dot
+from numpy.linalg import norm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tqdm import tqdm
 
 from .commons import Utils
-from .fasttext import FastText
 
 
-class QueryExpansion(FastText):
-    def __init__(self, fasttext_vectors: np.array, word_vectors: KeyedVectors, a1: list, dims: int = 100):
-        self.fasttext_vectors = fasttext_vectors
-        super().__init__(word_vectors, a1, dims)
+class TFIDF:
+    def __init__(self, a1: list):
+        self.docs = a1
+        self.vectorizer = TfidfVectorizer(
+            use_idf=True, norm='l2', ngram_range=(1, 1), analyzer='word')
+        self.vectorizer.fit_transform(self.docs)
+        cwd = Path(__file__).parent.resolve()
+        sw = cwd / Path('stopwords.txt')
+        with codecs.open(sw, 'r', 'utf-8') as f:
+            ls = f.readlines()
+        self.stopwords = [x.strip() for x in ls]
 
-    def query_expansion(self, query: str) -> str:
-        number_of_related_queries = 100
-        vector = self.get_fasttext(query)
-        vector /= np.linalg.norm(vector)
-        final_vector = np.matmul(self.fasttext_vectors, np.transpose(vector))
-        R = np.array(final_vector).argsort()[-number_of_related_queries:][::-1]
-        NR = np.array(final_vector).argsort()[:number_of_related_queries][::-1]
-        R_mean = np.mean(self.fasttext_vectors[R])
-        NR_mean = np.mean(self.fasttext_vectors[NR])
-        expanded_vector = vector + 1.5 * R_mean - 0.4 * NR_mean
-        expanded_vector /= np.linalg.norm(expanded_vector)
-        R_expanded = np.array(final_vector).argsort()[-1:][::-1]
-        expanded_query = self.docs[R_expanded]
-        return expanded_query
+    def forward(self, querry: str, k=10, is_vector=False, words=[]):
+        ten_tokens = [0] * k
+        ten_scores = [-100] * k
+
+        if not is_vector:
+            vector = self.vectorizer.transform([querry])
+            b = vector.toarray()
+            q1 = querry.split()
+        else:
+            vector = querry
+            b = [vector.tolist()]
+            q1 = words
+
+        NR = []
+        for item in tqdm(self.docs):
+            is_common = False
+            items = item.split()
+            for itemq in q1:
+                if itemq in items:
+                    is_common = True
+            if not is_common:
+                NR.append(item)
+                continue
+            vector = self.vectorizer.transform([item])
+            c = vector.toarray()
+            if (norm(c[0])*norm(b[0])) != 0:
+                cos_sim = dot(c[0], b[0])/(norm(c[0])*norm(b[0]))
+                if cos_sim != 0:
+                    for i in range(k):
+                        if ten_scores[i] < cos_sim:
+                            ten_scores[i] = cos_sim
+                            ten_tokens[i] = item
+                            break
+                    for i in range(k):
+                        for j in range(i, k):
+                            if ten_scores[i] > ten_scores[j]:
+                                ten_scores[i], ten_scores[j] = ten_scores[j], ten_scores[i]
+                                ten_tokens[i], ten_tokens[j] = ten_tokens[j], ten_tokens[i]
+        return ten_tokens, NR
+
+    def query_expansion(self, query: str)->list:
+        R, NR = self.forward(query)
+        NR = random.sample(NR, 10)
+        R_vecs = []
+        NR_vecs = []
+        words = []
+        for item in R:
+            items = item.split()
+            for itemq in items:
+                if itemq not in self.stopwords:
+                    words.append(itemq)
+            vector = self.vectorizer.transform([item])
+            c = np.array(vector.toarray()[0])
+            R_vecs.append(c)
+        words = random.sample(words, 6)
+        for oo in query.split():
+            words.append(oo)
+        R_vecs = np.array(R_vecs)
+        R_mean = np.mean(R_vecs, axis=0)
+        for item in NR:
+            vector = self.vectorizer.transform([item])
+            c = np.array(vector.toarray()[0])
+            NR_vecs.append(c)
+        NR_vecs = np.array(NR_vecs)
+        out1, _ = self.forward(query, 1)
+        vector = self.vectorizer.transform(out1)
+        a, _ = self.forward(R_mean, 1, True, words)
+        return a
 
 
 def query_expansion(query: str) -> str:
     cwd = Path(__file__).parent.resolve()
-    vector_file_path = cwd / Path('fasttext_vec_file.vec')
-    word_vectors = KeyedVectors.load_word2vec_format(vector_file_path)
     u = Utils(cwd)
     sen_v2 = u.get_data()
-    fasttext_vectors = np.loadtxt(cwd / Path('fasttext_vectors.txt'))
-    nan_indexes = []
-    for i in range(len(fasttext_vectors)):
-        if np.isnan(fasttext_vectors[i]).any():
-            nan_indexes.append(i)
-    fasttext_vectors = np.delete(fasttext_vectors, nan_indexes, axis=0)
-    sen_v2 = np.delete(sen_v2, nan_indexes, axis=0)
-    query_expander = QueryExpansion(fasttext_vectors, word_vectors, sen_v2)
+    query_expander = TFIDF(sen_v2)
     return query_expander.query_expansion(query)[0].strip().replace('\n', '')
