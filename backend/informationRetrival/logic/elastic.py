7e0d3f0fba9dc7bd4aa4f4ac5c9b722e35b6991d
AhmadZafarani <ahmadzafarani2013@gmail.com>
2022-07-27 10:16:00 +0430

    elastic search done

--- backend/informationRetrival/logic/elastic.py
+++ backend/informationRetrival/logic/elastic.py
@@ -1,33 +1,24 @@
-from pathlib import Path
-
 from elasticsearch import Elasticsearch
-from .commons import *
 
 
 class Elastic:
-    def __init__(self, a1: list):
-        self.docs = a1
-
     def forward(self, query: str, k: int = 10) -> list:
         client = Elasticsearch("http://localhost:9200")
         search_param = {
             "match": {
-                "mesra": "" + query + ""
+                "Beyt": "" + query + ""
             }
         }
 
-        mesras = []
+        beyts = []
         response = client.search(
-            index=self.docs,  query=search_param, size=k)
+            index="shahnameh",  query=search_param, size=k)
         for hit in response['hits']['hits']:
-            mesras.append(hit["_source"])
-        return mesras
+            beyts.append(hit["_source"]["Beyt"].replace("##", "\t\t\t"))
+        return beyts
 
 
 def get_elastic_search_query_results(query: str, k: int = 10) -> list:
-    cwd = Path(__file__).parent.resolve()
-    u = Utils(cwd)
-    sen_v2 = u.get_data()
-    b1 = Elastic(sen_v2)
+    b1 = Elastic()
     results = b1.forward(query, k)
-    return u.get_beyts_by_mesras(results)
+    return results
